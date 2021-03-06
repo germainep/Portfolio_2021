c962edfef769ede12fb74437d7a872d9e9303666
Germaine P <me@germainep.com>
2021-03-06 16:32:20 -0500

    config: updated config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.md"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      accent: "#59d8e6",
+      primary: "#106fc0",
+      textColor: "#44464A",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
