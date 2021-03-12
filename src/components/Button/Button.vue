a53aeb56878ce840bd79edcd3f7e6919a9de16fc
Germaine P <me@germainep.com>
2021-03-12 15:35:43 -0500

    fix created button component to make using button a bit easier.

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class='self-center bg-accent-500 shadow-md shadow-lg rounded-lg p-5 text-center'>
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+}
+</script>
---
+++ src/components/SkillsBox/SkillsBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='SkillsBox grid grid-cols-3 grid-rows-2 bg-white w-2/3'>
+    <div class='col-span-3 grid grid-col-3 bg-primary-600 w-full'>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skillsbox',
+}
+</script>
