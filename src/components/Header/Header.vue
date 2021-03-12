efe7bba560a56f87b6c72b6ce50cc7827f749b22
Germaine P <me@germainep.com>
2021-03-12 15:38:10 -0500

    fix moved unnecessary class from the header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,15 +1,16 @@
 <template>
-    <header class="bg-primary relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
-      <div class="logo">
+    <header class='bg-primary-500 relative flex flex-wrap items-center justify-between px-2 py-3'>
+      <div class='logo'>
       </div>
       <Navigation/>
     </header>
 </template>
 
 <script>
-    import Navigation from '~/components/Navigation/Navigation'
-    export default {
-        name: 'Header',
-      components: {Navigation}
-    };
+import Navigation from '~/components/Navigation/Navigation'
+
+export default {
+  name: 'Header',
+  components: {Navigation},
+}
 </script>
