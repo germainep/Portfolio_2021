483e164db56a2429fbdd3f802f7d5b9e7a6d2c70
Germaine P <me@germainep.com>
2021-03-19 12:10:25 -0400

    feat: created some test data for use in the card component

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='Card grid grid-rows-2 rounded-lg'>
+    <div>
+      <g-image src=''></g-image>
+      <h1></h1>
+    </div>
+
+    <div class='rounded-lg bg-primary-800'>
+      <p class='text-accent-100'></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+}
+</script>
