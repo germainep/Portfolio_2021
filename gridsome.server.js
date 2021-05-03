adfe255a6e528243705a43a57347aa70b7b664cc
Germaine P <me@germainep.com>
2021-05-03 19:30:50 -0400

    fix: moved images to the correct static directory

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,11 +6,24 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(({ addCollection, addSchemaTypes, addSchemaResolvers }) => {
+    addSchemaTypes(`
+    type Picture implements Node @infer {
+      src: Picture
+    }
+    `);
+    addSchemaResolvers({
+      Picture: {
+        src(picture, _args, context, info) {
+          console.log(picture.image, context);
+          return `${picture.image.src}`;
+        },
+      },
+    });
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-}
+  });
+};
