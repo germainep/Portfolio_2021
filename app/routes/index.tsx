68636c41a5f04da3ce215e577dd6a17e6a1d1881
Germaine P <me@germainep.com>
2022-06-28 17:11:00 -0400

    style(landing-page): adjusting styling to be pixel perfect
    changing sizing and margins to match the design perfectly

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,13 +5,11 @@ export default function Index() {
     <div>
       <div className="container grid md:grid-cols-2  mx-auto mt-32">
         <div className="mx-auto px-4">
-          <h1 className="text-5xl text-primary font-bold mb-1.5 mx-auto">
-            Germaine P
-          </h1>
-          <h2 className="text-3xl text-secondary font-medium mb-5 mx-auto">
+          <h1 className="text-6xl text-primary  mx-auto">Germaine P</h1>
+          <h2 className="text-4xl text-secondary font-medium mb-36 mx-auto">
             Full-Stack Software Engineer
           </h2>
-          <p className="text-text md:w-2/3 md:mx-0 mx-auto px-2">
+          <p className="text-white text-xl md:w-2/3 md:mx-0 mx-auto px-2">
             Hi, Thanks for visiting. I’m Germaine, a 36 year old Full Stack Web
             Developer. I build interfaces using Javascript technology stacks
             (any javascript stack). I currently work freelance but I’m open to
@@ -22,8 +20,8 @@ export default function Index() {
             Resume
           </button>
         </div>
-        <div className="container mx-auto md:mt-0 m-36">
-          <h2 className="text-center text-primary text-5xl">Recent Projects</h2>
+        <div className="container mx-auto md:mt-0 m-32">
+          <h2 className="text-center text-primary text-6xl">Recent Projects</h2>
           <div className="grid grid-flow-row">
             <div className="my-10 rounded mx-auto w-8/12">
               <img src={diamond} alt="Diamond Luxe detailing project" />
