f26f5ceba0a104c3a32d2ec7519e3bf1636927cd
Germaine P <me@germainep.com>
2022-07-04 10:17:33 -0400

    feat(landing-page): Added a loader for recent projects
    Added a loader to be able to update recent projects from the backend in the future

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,25 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
 import diamond from "../images/diamond-luxe.png";
 
-export default function Index() {
+export const loader: LoaderFunction = async () => {
+  return json([
+    {
+      id: 1,
+      name: "Diamond Luxe Detailing",
+      description: "Diamond Luxe Detailing website",
+      image: diamond,
+    },
+  ]);
+};
+export default function Home() {
+  const projects = useLoaderData();
   return (
     <div>
-      <div className="container grid md:grid-cols-2  mx-auto mt-32">
-        <div className="mx-auto px-4">
+      <div className="container grid md:grid-cols-2  mx-auto">
+        <div className="mx-auto px-4 mt-44">
           <h1 className="text-6xl text-primary  mx-auto">Germaine P</h1>
           <h2 className="text-4xl text-secondary font-medium mb-36 mx-auto">
             Full-Stack Software Engineer
@@ -16,16 +31,19 @@ export default function Index() {
             the right staff position. For more information download my resume
             below.
           </p>
-          <button className="bg-primary hover:bg-secondary py-1 px-2 rounded-lg text-lg mt-32 w-32 h-10">
+          <button className="bg-primary hover:bg-secondary py-1 px-2 rounded-lg text-lg mt-16 w-32 h-10 drop-shadow-md">
             Resume
           </button>
         </div>
-        <div className="container mx-auto md:mt-0 m-32">
+        <div className="container mx-auto mt-24">
           <h2 className="text-center text-primary text-6xl">Recent Projects</h2>
           <div className="grid grid-flow-row">
-            <div className="my-10 rounded mx-auto w-8/12">
-              <img src={diamond} alt="Diamond Luxe detailing project" />
-            </div>
+            {projects.map((project) => (
+              <div key={project.id} className="my-10 rounded mx-auto w-64">
+                <img src={project.image} alt={project.title} />
+                <p className="text-white">{project.description}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
