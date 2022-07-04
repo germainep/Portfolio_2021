21f888de8d07dd41d2b556c6ca97cfa96109970a
Germaine P <me@germainep.com>
2022-07-04 10:17:33 -0400

    feat(landing-page): Added a loader for recent projects
    Added a loader to be able to update recent projects from the backend in the future

--- app/components/Navigation/Navbar.tsx
+++ app/components/Navigation/Navbar.tsx
@@ -1,27 +1,12 @@
-import { Link } from "@remix-run/react";
+import Navlink from "~/components/Navigation/Navlink";
 
 function Navbar() {
   return (
     <>
       <nav className="flex justify-end align-center mr-4 h-24 py-9">
-        <Link
-          to="/"
-          className="text-2xl text-primary font-medium hover:text-secondary active:underline"
-        >
-          Home
-        </Link>
-        <Link
-          to="/portfolio"
-          className="mx-4 text-2xl text-primary font-medium  hover:text-secondary"
-        >
-          Projects
-        </Link>
-        <Link
-          to="/blog"
-          className="text-primary text-2xl font-medium hover:text-secondary mr-4"
-        >
-          Blog
-        </Link>
+        <Navlink url="/" name="Home" />
+        <Navlink url="/portfolio" name="Projects" />
+        <Navlink url="/blog" name="Blog" />
       </nav>
     </>
   );
--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,8 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type {
+  ErrorBoundaryComponent,
+  LinksFunction,
+  MetaFunction,
+} from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,7 +11,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import Navigation from "~/components/Navigation";
+
+import Navigation from "~/components/Navigation/Navbar";
 
 import styles from "./styles/app.css";
 
@@ -18,6 +23,26 @@ export const meta: MetaFunction = () => ({
 });
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
+  console.error(error);
+  return (
+    <html>
+      <head>
+        <title>Germaine P. Error</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-background">
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+        <Scripts />
+      </body>
+    </html>
+  );
+};
 
 export default function App() {
   return (
